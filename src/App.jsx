c8b5bb4b1627c08c085b6cceaff06fd4aae194ee
far1h <farihmuhammad45@gmail.com>
2024-10-13 13:47:43 +0700

    Pass Functions as Values to Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { CORE_CONCEPTS } from './data.js';
-import CoreConcept from './components/CoreConcept.jsx';
-import Header from './components/Header.jsx';
-import TabButton from './components/TabButton.jsx';
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("Hello World - selected!");
+  }
+
   return (
     <div>
       <Header></Header>
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept
-           title={CORE_CONCEPTS[0].title} 
-           description={CORE_CONCEPTS[0].description} 
-           image={CORE_CONCEPTS[0].image}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            ></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
